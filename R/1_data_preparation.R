faf844d679cfca50fcc81beb8272ad2a532b70c1
DAChristina <d.christina23@imperial.ac.uk>
2025-03-16 22:34:21 +0700

    multivariable test

--- R/1_data_preparation.R
+++ R/1_data_preparation.R
@@ -5,25 +5,63 @@ source("global/fun.R")
 df_epi_lombok <- readxl::read_excel("raw_data/DATABASE PENELITIAN PNEUMOKOKUS (Manado, Lombok, Sorong, Sumbawa).xlsx",
                                  sheet = "Lombok") %>% 
   dplyr::rename_all(~stringr::str_replace_all(., " ", "_")) %>% 
-  dplyr::left_join(readxl::read_excel("raw_data/Data WGS_Lombok.xlsx", sheet = "Sheet1") %>% 
-                     dplyr::rename_all(~stringr::str_replace_all(., " ", "_")) %>% 
-                     dplyr::rename(SPECIMEN_ID = No_Isolat),
-                   by = "SPECIMEN_ID") %>% 
   dplyr::mutate(SPECIMEN_ID = gsub(" ", "_", SPECIMEN_ID),
-                area = "Lombok")
+                area = "Lombok",
+                across(where(is.character), ~ na_if(., "N/A")),
+                across(where(is.character), ~ if_else(. == "-", "tidak", .)),
+                # across(everything(), as.character),
+                across(everything(), tolower)) %>% 
+  dplyr::select(-contains("koding"),-contains("Ya="))
+
+write.csv(df_epi_lombok, "raw_data/temporary_df_epi_lombok.csv",
+          row.names = F)
 
 df_epi_sumbawa <- readxl::read_excel("raw_data/DATABASE PENELITIAN PNEUMOKOKUS (Manado, Lombok, Sorong, Sumbawa).xlsx",
                                     sheet = "Sumbawa") %>% 
   dplyr::rename_all(~stringr::str_replace_all(., " ", "_")) %>% 
   dplyr::mutate(SPECIMEN_ID = gsub("-", "_", SPECIMEN_ID),
-                area = "Sumbawa")
+                area = "Sumbawa",
+                across(where(is.character), ~ na_if(., "N/A")),
+                across(where(is.character), ~ if_else(. == "-", "tidak", .)),
+                # across(everything(), as.character),
+                across(everything(), tolower)) %>% 
+  dplyr::select(-contains("koding"),-contains("Ya="))
+
+write.csv(df_epi_sumbawa, "raw_data/temporary_df_epi_sumbawa.csv",
+          row.names = F)
+
+setdiff(names(df_epi_lombok), names(df_epi_sumbawa))
+setdiff(names(df_epi_sumbawa), names(df_epi_lombok))
+
 
 # pending
 # column types need to be corrected and re-checked
-df_epi_ls <- dplyr::bind_rows(df_epi_lombok %>% 
-                                dplyr::mutate("koding_jenis_atap_Rumah_(_Seng=_1,_Asbes=2_,_Beton=3__,_Kayu=4,_Genteng=_5,_Spandek=6,_lain-lain_(Jerami,_daun_palem)=7" = as.numeric("koding_jenis_atap_Rumah_(_Seng=_1,_Asbes=2_,_Beton=3__,_Kayu=4,_Genteng=_5,_Spandek=6,_lain-lain_(Jerami,_daun_palem)=7")),
-                              df_epi_sumbawa %>% 
-                                dplyr::mutate("koding_jenis_atap_Rumah_(_Seng=_1,_Asbes=2_,_Beton=3__,_Kayu=4,_Genteng=_5,_Spandek=6,_lain-lain_(Jerami,_daun_palem)=7" = as.numeric("koding_jenis_atap_Rumah_(_Seng=_1,_Asbes=2_,_Beton=3__,_Kayu=4,_Genteng=_5,_Spandek=6,_lain-lain_(Jerami,_daun_palem)=7")))
+df_epi_ls <- dplyr::bind_rows(df_epi_lombok #%>% 
+                                # dplyr::select(-contains("koding"))
+                              , 
+                              df_epi_sumbawa #%>% 
+                                # dplyr::select(-contains("koding"))
+                                ) %>% 
+  dplyr::mutate(across(where(is.character), ~ na_if(., "N/A")))
+                                
+# Test weird unique value
+lapply(df_epi_ls, unique)
+
+sapply(df_epi_ls, unique)
+
+df_epi_ls_summarise <- df_epi_ls %>% 
+  dplyr::summarise(across(everything(), ~ list(as.data.frame(table(.))))) %>% 
+  tidyr::unnest(cols = everything())
+
+view(t(df_epi_ls_summarise))
+
+# Select columns
+
+
+
+
+
+
 
 # test available fasta
 df_confirm_lombok <- readxl::read_excel("raw_data/DATABASE PENELITIAN PNEUMOKOKUS (Manado, Lombok, Sorong, Sumbawa).xlsx",
@@ -74,14 +112,62 @@ area_suspect_report <- df_epi_test %>%
   view()
 
 
-write.csv(df_epi_test, "outputs/temporary_available_fasta_list_in_DC.csv",
+write.csv(df_epi_test, "report/temporary_available_fasta_list_in_DC.csv",
          row.names = F)
 # excel report
 writexl::write_xlsx(list(Sheet1 = df_epi_test,
                          Sheet2 = area_report,
                          Sheet3 = area_suspect_report),
-                    "outputs/temporary_available_fasta_list_in_DC.xlsx")
+                    "report/temporary_available_fasta_list_in_DC.xlsx")
 
+
+df_epi_test <- read.csv("report/temporary_available_fasta_list_in_DC.csv") %>% 
+  dplyr::mutate(
+    final_pneumo_decision = case_when(
+      WGS_Result...11 == "Streptococcus pneumoniae" ~ "Positive",
+      S._pneumoniae_suspect_culture_colony == "Yes" & S._pneumoniae_culture_result == "Pos" & WGS_Result...11 == "Streptococcus pneumoniae" ~ "Positive",
+      S._pneumoniae_suspect_culture_colony == "Yes" & S._pneumoniae_culture_result == "Pos" & WGS_Result...11 != "Streptococcus pneumoniae" ~ "Negative",  # OR "Failed_to_be_extracted"?
+      TRUE ~ S._pneumoniae_culture_result
+    ),
+    final_pneumo_decision = case_when(
+      final_pneumo_decision == "Neg" ~ "Negative",
+      is.na(final_pneumo_decision) ~ "Negative",
+      TRUE ~ final_pneumo_decision  # Ensure other values remain unchanged
+    )
+  )
+
+# Quick viz serotypes
+# Compute percentage
+df_serotype_summary <- df_epi_test %>% 
+  dplyr::filter(!is.na(SEROTYPE_.WGS.)) %>% 
+  dplyr::count(SEROTYPE_.WGS.) %>%
+  dplyr::mutate(Percentage = n / sum(n) * 100)
+
+ggplot(df_serotype_summary, aes(x = SEROTYPE_.WGS., y = Percentage, fill = SEROTYPE_.WGS.)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = paste0(round(Percentage, 1), "%")), vjust = -0.5) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  labs(x = "Category", y = "Percentage", 
+       title = "All Serotypes") +
+  theme_bw() +
+  theme(legend.position = "none")
+
+# Compute percentage by area
+df_serotype_area_summary <- df_epi_test %>% 
+  dplyr::filter(!is.na(SEROTYPE_.WGS.)) %>% 
+  dplyr::group_by(area, SEROTYPE_.WGS.) %>%
+  dplyr::summarise(Count = n(), .groups = "drop") %>%
+  dplyr::mutate(Percentage = Count / sum(Count) * 100)
+
+ggplot(df_serotype_area_summary, aes(x = SEROTYPE_.WGS., y = Percentage, fill = SEROTYPE_.WGS.)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = paste0(round(Percentage, 1), "%")), vjust = -0.5) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  labs(x = "Category", y = "Percentage", 
+       title = "All Serotypes") +
+  theme_bw() +
+  theme(legend.position = "none") +
+  facet_wrap(~ area)
 
 # right_join to see undetected 49 fasta files
 df_epi_test_right_join <- dplyr::bind_rows(
@@ -171,5 +257,30 @@ df_test <- df_epi %>%
 # try OR report works only for categorical data
 or_matrix_all <- generate_or_matrix_report(df_input = df_test, binary_disease = "Oral.Cancer..Diagnosis.")
 
+or_matrix_table_report <- purrr::map_dfr(names(or_matrix_all), function(var) {
+  df <- or_matrix_all[[var]]$measure
+  if (is.null(df)) return(NULL)
+  df <- as.data.frame(df) # annoying df convertion
+  
+  df <- df %>% 
+    dplyr::mutate(Aspect = var, Category = rownames(df)) 
+  
+  rownames(df) <- NULL
+  return(df)
+})
 
-# try glm OR report for numerical data
+# try glm OR report
+or_logistic_all <- generate_glm_logistic_report(df_input = df_test, binary_disease = "Oral.Cancer..Diagnosis.")
+
+or_logistic_table_report <- purrr::imap_dfr(or_logistic_all, ~{
+  dplyr::tibble(
+    Names = names(.x$odds_ratio),
+    Odds_Ratio = .x$odds_ratio,
+    Category = .y
+  )
+}) %>% 
+  dplyr::arrange(Category)
+
+
+# try glm multivariable OR report
+or_multivariable_all <- generate_glm_multivariable_report(df_input = df_test, binary_disease = "Oral.Cancer..Diagnosis.")
